cdae7976f329f3b8502d8a7d43b9bee250d196dd
qihua.li <qihua.li@wehotelglobal.com>
2020-01-08 18:08:59 +0800

    liqihua

--- catering-user/src/pages/purchase/index.js
+++ catering-user/src/pages/purchase/index.js
@@ -12,103 +12,37 @@ class Purchase extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            visualList: [],
             categoryList: [
                 {
                     categoryName: '新鲜水果',
-                    productList: [
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        }
-                    ]
+                    productList: [],
+                    productImg: null
                 },
                 {
                     categoryName: '鲜榨果汁',
-                    productList: [
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        }
-                    ]
+                    productList: [],
+                    productImg: null
                 },
                 {
                     categoryName: '找好茶',
-                    productList: [
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        },
-                        {
-                            title: '(大杯)波霸奶茶',
-                            intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
-                            price: 18.88
-                        }
-                    ]
+                    productList: [],
+                    productImg: null
                 },
                 {
                     categoryName: '找口感',
-                    productList: []
+                    productList: [],
+                    productImg: null
                 },
                 {
                     categoryName: '一层热销水果蛋糕',
-                    productList: []
+                    productList: [],
+                    productImg: null
                 },
                 {
                     categoryName: '三层热销蛋糕',
-                    productList: []
+                    productList: [],
+                    productImg: null
                 }
             ]
         }
@@ -137,30 +71,19 @@ class Purchase extends React.Component {
                         }
                     </nav>
                     <div className='menu-product' ref='productContainer' onScrollCapture={this.handleProductScroll.bind(this)}>
-                        <div className='menu-product-wrap' ref='productWrap'>
+                        <div id='menu-product-wrap' className='menu-product-wrap' ref='productWrap'>
                         {
                             this.state.categoryList.map( (category,categoryIndex) => {
                                 return (
-                                    <div className='product-list' key={categoryIndex}>
+                                    <div className='product-list' key={categoryIndex} title={category.categoryName}>
                                         <dl>
                                             <dt>{category.categoryName}</dt>
                                             {
                                                 category.productList.map( (product,productIndex) => {
-                                                    var productImg = null;
-                                                    const imgIndex = productIndex % 4
-                                                    if(imgIndex === 0) {
-                                                        productImg = imgProductPicture1;
-                                                    } else if(imgIndex === 1) {
-                                                        productImg = imgProductPicture2;
-                                                    } else if(imgIndex === 2) {
-                                                        productImg = imgProductPicture3;
-                                                    } else if(imgIndex === 3) {
-                                                        productImg = imgProductPicture4;
-                                                    }
                                                     return (
                                                         <dd key={productIndex}>
                                                             <div className='product-picture'>
-                                                                <img src={productImg} alt=''/>
+                                                                <img src={product.productImg} alt=''/>
                                                             </div>
                                                             <div className='product-content'>
                                                                 <div className='product-title'>{product.title}</div>
@@ -191,33 +114,64 @@ class Purchase extends React.Component {
         );
     }
 
+    componentWillMount() {
+        // 测试数据
+        var productIndex = 0
+        var newCategoryList = []
+        this.state.categoryList.map((category) => {
+            var newCategory = {... category}
+            newCategory.productList = []
+            for(var i=0; i<10; i++) {
+                var productImg = null
+                const imgIndex = productIndex % 4
+                if(imgIndex === 0) {
+                    productImg = imgProductPicture1;
+                } else if(imgIndex === 1) {
+                    productImg = imgProductPicture2;
+                } else if(imgIndex === 2) {
+                    productImg = imgProductPicture3;
+                } else if(imgIndex === 3) {
+                    productImg = imgProductPicture4;
+                }
+                newCategory.productList.push(
+                    {
+                        title: '(大杯)波霸奶茶',
+                        intro: '四季春茶搭配特选植脂末，经由黄金比例调制而成，香滑顺口。主要原料：四季春茶；辅料：植脂末。',
+                        price: 18.88,
+                        productImg: productImg
+                    }
+                )
+                productIndex += 1
+            }
+            newCategoryList.push(newCategory)
+        })
+        this.setState({
+            categoryList: newCategoryList
+        })
+    }
+
     componentDidMount() {
-        window.addEventListener('scroll',this.handleScroll.bind(this))
+        var visualList = [];
+        var h = 0;
+        document.getElementById('menu-product-wrap').childNodes.forEach((category) => {
+            var visual = new Object();
+            visual.min = h;
+            h += category.clientHeight;
+            visual.max = h;
+            visual.title = category.getAttribute('title');
+            visualList.push(visual);
+        })
+        this.setState({
+            visualList: visualList
+        })
     }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll',this.handleScroll.bind(this))
-    }
-
-    handleScroll = (e) => {
-        //console.log(e)
-    }
-
+    
     handleProductScroll = (e) => {
-        console.log('productWrap offsetHeight:' + this.refs.productWrap.offsetHeight)
-        console.log('productWrap offsetTop:' + this.refs.productWrap.offsetTop)
-        console.log('productWrap clientHeight:' + this.refs.productWrap.clientHeight)
-        console.log('productWrap clientTop:' + this.refs.productWrap.clientTop)
-        console.log('productWrap scrollHeight:' + this.refs.productWrap.scrollHeight)
-        console.log('productWrap scrollTop:' + this.refs.productWrap.scrollTop)
-
-        console.log('productContainer offsetHeight:' + this.refs.productContainer.offsetHeight)
-        console.log('productContainer offsetTop:' + this.refs.productWrap.offsetTop)
-        console.log('productContainer clientHeight:' + this.refs.productWrap.clientHeight)
-        console.log('productContainer clientTop:' + this.refs.productWrap.clientTop)
-        console.log('productContainer scrollHeight:' + this.refs.productWrap.scrollHeight)
-        console.log('productContainer scrollTop:' + this.refs.productWrap.scrollTop)
-        console.log('----------------')
+        this.state.visualList.map((visual) => {
+            if(this.refs.productContainer.scrollTop > visual.min && this.refs.productContainer.scrollTop <= visual.max) {
+                console.log('当前菜单:'+visual.title)
+            }
+        })
     }
     
 }
